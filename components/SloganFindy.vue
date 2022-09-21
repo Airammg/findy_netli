1cd873d037f1efdefe0b766f84a899418486d27d
Airam <Airam.marting@gmail.com>
2022-09-21 19:11:44 +0100

    Working on NuxtLink animation

--- components/SloganFindy.vue
+++ components/SloganFindy.vue
@@ -13,7 +13,26 @@
 }
 
 .look-link {
+  /* position: fixed; */
   color: #a8b030;
   text-decoration: none;
+  animation-name: my-animation;
+  animation-duration: 3s;
+  animation-direction: alternate;
+  animation-iteration-count: infinite;
+
+  /* animation-timing-function: linear; */
+
+  /* animation: ; */
+}
+
+@keyframes my-animation {
+  from {
+    box-shadow: 0 3px 300px -200px rgb(168 176 48 / 100%);
+  }
+
+  to {
+    box-shadow: 0 0 0 2px rgb(168 176 48 / 100%);
+  }
 }
 </style>
